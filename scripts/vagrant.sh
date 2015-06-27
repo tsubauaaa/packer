5767108161e10297a37f51e2baaf1de758a0a254
Tsubasa Hirota <tsubasa11@marble.ocn.ne.jp>
2015-06-27 23:57:32 +0900

    qemu add

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ "$PACKER_BUILDER_TYPE" == "virtualbox-iso" ] || [ "$PACKER_BUILDER_TYPE" == "vmware-iso" ] ; then
+if [ "$PACKER_BUILDER_TYPE" == "virtualbox-iso" ] || [ "$PACKER_BUILDER_TYPE" == "vmware-iso" ] || [ "$PACKER_BUILDER_TYPE" == "qemu" ; then
   /bin/mkdir -pm 700 /home/vagrant/.ssh
   /usr/bin/curl -L 'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub' > "/home/vagrant/.ssh/authorized_keys"
   /bin/chmod 0600 /home/vagrant/.ssh/authorized_keys
