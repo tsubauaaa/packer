2006a97e5f802c06e57776f6a130f25c2f1306a3
Tsubasa Hirota <tsubasa11@marble.ocn.ne.jp>
2015-06-30 20:04:04 +0900

    update

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,7 +2,7 @@
 /usr/bin/yum -y clean all
 /bin/rm -rf /tmp/*
 /bin/rm -rf /var/tmp/*
-if [ "$PACKER_BUILDER_TYPE" == "virtualbox-iso" ] || [ "$PACKER_BUILDER_TYPE" == "vmware-iso" ] ; then
+if [ "$PACKER_BUILDER_TYPE" == "virtualbox-iso" ] || [ "$PACKER_BUILDER_TYPE" == "vmware-iso" ] || [ "$PACKER_BUILDER_TYPE" == "qemu" ] ; then
   /bin/rm -rf /etc/udev/rules.d/70-persistent-net.rules
   /bin/rm -rf /etc/udev/rules.d/70-persistent-cd.rules
   /bin/rm -rf /lib/udev/rules.d/75-persistent-net-generator.rules
