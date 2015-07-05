27718389fe639c664498320c2b300cd17d185627
Tsubasa Hirota <tsubasa11@marble.ocn.ne.jp>
2015-07-05 21:41:13 +0900

    update

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-/bin/mkdir -pm 700 /home/vagrant/.ssh
-/usr/bin/curl -L 'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub' > "/home/vagrant/.ssh/authorized_keys"
-/bin/chmod 0600 /home/vagrant/.ssh/authorized_keys
-/bin/chown -R vagrant:vagrant /home/vagrant/.ssh
+if [ "$PACKER_BUILDER_TYPE" == "virtualbox-iso" ] || [ "$PACKER_BUILDER_TYPE" == "vmware-iso" ] || [ "$PACKER_BUILDER_TYPE" == "qemu" ; then
+  /bin/mkdir -pm 700 /home/vagrant/.ssh
+  /usr/bin/curl -L 'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub' > "/home/vagrant/.ssh/authorized_keys"
+  /bin/chmod 0600 /home/vagrant/.ssh/authorized_keys
+  /bin/chown -R vagrant:vagrant /home/vagrant/.ssh
+else
+  exit 0
+fi
